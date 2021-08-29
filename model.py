fa612d1625f7398242255db2686f6d75a9dd0c11
Lee, Jun Seok <lego3410@gmail.com>
2021-08-29 20:58:21 +0900

    Add acc

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchmetrics
+import torchmetrics as metrics
 import pytorch_lightning as pl
 
 
@@ -13,6 +13,8 @@ class Classifier(pl.LightningModule):
             nn.Linear(64, 32), nn.ReLU(),
             nn.Linear(32, 10), nn.Softmax()
         )
+        self.train_acc = metrics.Accuracy(compute_on_step=False)
+        self.valid_acc = metrics.Accuracy(compute_on_step=False)
 
     def forward(self, x):
         logits = self.layers(x)
@@ -23,15 +25,25 @@ class Classifier(pl.LightningModule):
         x = x.view(x.shape[0], -1)
         logits = self(x)
         loss = F.cross_entropy(logits, y)
-        self.log('train_loss', loss)
+        self.log('train loss', loss)
+        self.train_acc(logits, y)
         return loss
+
+    def training_epoch_end(self, outputs):
+        self.log('train accuracy', self.train_acc.compute())
+        self.train_acc.reset()
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         x = x.view(x.shape[0], -1)
         logits = self(x)
         loss = F.cross_entropy(logits, y)
-        self.log('valid_loss', loss)
+        self.log('valid loss', loss)
+        self.valid_acc(logits, y)
+
+    def validation_epoch_end(self, outputs):
+        self.log('valid accuracy', self.valid_acc.compute())
+        self.valid_acc.reset()
 
     def configure_optimizers(self):
         return torch.optim.AdamW(self.parameters(), lr=3e-4)
