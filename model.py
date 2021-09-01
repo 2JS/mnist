2c3dbd4322b6d5a6c619105ac57f7030859c463e
Lee, Jun Seok <lego3410@gmail.com>
2021-09-02 00:47:11 +0900

    Add dropout

--- model.py
+++ model.py
@@ -53,26 +53,33 @@ class ConvClassifier(pl.LightningModule):
         super().__init__()
         self.layers = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
+            nn.Dropout2d(p=0.1, inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
+            nn.Dropout2d(p=0.1, inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
 
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
+            nn.Dropout2d(p=0.1, inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
+            nn.Dropout2d(p=0.1, inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
 
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
+            nn.Dropout2d(p=0.1, inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
+            nn.Dropout2d(p=0.1, inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
 
             nn.Flatten(),
             nn.Linear(4096, 512),
-            nn.ReLU(),
+            nn.Dropout(p=0.1, inplace=True),
+            nn.ReLU(inplace=True),
             nn.Linear(512, 10),
         )
         self.train_acc = metrics.Accuracy()
