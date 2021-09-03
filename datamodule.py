553295924791ae4755ff4ad4d19e57a1c37b6009
Lee, Jun Seok <lego3410@gmail.com>
2021-09-04 02:16:37 +0900

    Batch size 2**7

--- datamodule.py
+++ datamodule.py
@@ -27,7 +27,7 @@ class CIFAR10DataModule(pl.LightningDataModule):
         )
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=2**5, num_workers=os.cpu_count(), shuffle=True)
+        return DataLoader(self.train_dataset, batch_size=2**7, num_workers=os.cpu_count(), shuffle=True)
     
     def val_dataloader(self):
         return DataLoader(self.valid_dataset, batch_size=2**7, num_workers=os.cpu_count())
