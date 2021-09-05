6b784c4bd2e4b98ecf9f1c3fc1caeb7d84efaa69
Lee, Jun Seok <lego3410@gmail.com>
2021-09-05 09:38:39 +0900

    Load from 1pgkfk53:v0

--- cifar10.py
+++ cifar10.py
@@ -4,16 +4,13 @@ import torch
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+from pathlib import Path
 
 from model import *
 from datamodule import CIFAR10DataModule
 
 if __name__=='__main__':
     pl.seed_everything(0)
-
-    dm = CIFAR10DataModule()
-
-    model = ConvClassifier()
 
     logger = WandbLogger(project='cifar10', log_model=True)
 
@@ -22,6 +19,16 @@ if __name__=='__main__':
         gpus=-1,
         logger=logger,
     )
+
+    checkpoint_reference = 'teamvkik/cifar10/model-1pgkfk53:v0'
+    run = trainer.logger.experiment
+    artifact = run.use_artifact(checkpoint_reference, type='model')
+    artifact_dir = artifact.download()
+
+    dm = CIFAR10DataModule()
+
+    model = ConvClassifier.load_from_checkpoint(Path(artifact_dir)/'model.ckpt')
+
     logger.watch(model)
 
     trainer.fit(model, dm)
